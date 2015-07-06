7fde431c5e3dc4078c0648e1caea496c2c94c011
lodovica <lodo.marchesi@gmail.com>
2015-07-06 15:16:42 +0200

    hhhh

--- negozio/php/Settings.php
+++ negozio/php/Settings.php
@@ -30,7 +30,7 @@ class Settings {
                     break;
                 case 'spano.sc.unica.it':
                     // configurazione pubblica
-                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2015/marchesiLodovica/';
+                    self::$appPath = 'http://' . $_SERVER['HTTP_HOST'] . '/amm2015/marchesiLodovica/negozio';
                     break;
 
                 default:
