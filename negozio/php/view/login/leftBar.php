ada9451002bfcc034647bfe5072267b19d037e30
lodovica <lodo.marchesi@gmail.com>
2015-07-07 13:39:58 +0200

    Revert "Revert "error""
    This reverts commit 93967d5f865b9ff854ae0e9baba71fc8ec27fc22.

--- negozio/php/view/login/leftBar.php
+++
@@ -1,7 +0,0 @@
-<h2>Istruzioni</h2>
-<p>
-    Accesso al sistema.
-</p>
-<p>
-    Autenticarsi tramite l'apposito modulo per poter usufruire delle funzioni del sito.
-</p>
--- negozio/php/view/operatore/logo.php
+++
@@ -1,7 +0,0 @@
-
-<div id ="logout">
-    <p>Gestione operatore - <?= $user->getNome().' '.$user->getCognome() ?></p>
-    <p class="logout">
-        <a href="operatore?cmd=logout">Logout</a>
-    </p>
-</div>
