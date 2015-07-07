7ea99fd887e012f3ea1ccd9f6f5ffa5cb12d8928
lodovica <lodo.marchesi@gmail.com>
2015-07-07 13:39:41 +0200

    error

--- negozio/php/view/cliente/logo.php
+++
@@ -1,6 +0,0 @@
-<div id ="logout">
-    <p>Gestione Cliente - <?= $user->getNome().' '.$user->getCognome() ?></p>
-    <p class="logout">
-        <a href="cliente?cmd=logout">Logout</a>
-    </p>
-</div>
